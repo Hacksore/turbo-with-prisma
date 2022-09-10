554840853d91e090557f9f05ef0dd89bc2a2ecf6
Sean Boult <996134+Hacksore@users.noreply.github.com>
2022-09-10 02:52:18 +0000

    Init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,19 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
-const path = require('path')
-
-module.exports = {
+const config = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /prisma-client\/index\.js$/,
-      loader: 'string-replace-loader',
-      options: {
-        search: 'config.dirname = dirname',
-        replace: `config.dirname = '${path.dirname(require.resolve('database/generated/prisma-client'))}'`,
-      }
-    })
-    return config
-  }
+  // how can we not use the webpack hack?
 };
+
+export default config;
